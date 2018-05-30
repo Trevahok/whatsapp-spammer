8b34c6fe9042e741df8e86e664683fe003374506
Trevahok <vighneshss@gmail.com>
2018-05-30 09:07:15 +0530

    working code

---
+++ whatsappweb.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+web = webdriver.Chrome('/Users/ssvighnesh/chromedriver')
+web.get('http://web.whatsapp.com')
+name=input('enter the victim name:')
+message=input('enter ur message:')
+times=int(input('enter the number of times u want me to send this:'))
+try:
+    ck=web.find_element_by_xpath('//*[@title="{}"]'.format(name))
+    ck.click()
+    textfield=web.find_elements_by_class_name("_2S1VP")
+    for i in range(times):
+        textfield[0].send_keys(message+Keys.ENTER)
+except Exception as e:
+    print(e)
+web.close()
